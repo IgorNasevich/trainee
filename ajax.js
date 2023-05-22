f321da397102d10c37975417de8361e7dcb5cc17
IgorNasevich <igor.nasevich007@gmail.com>
2023-05-22 17:33:14 +0300

    feat: add timer renaming

--- ajax.js
+++ ajax.js
@@ -45,3 +45,21 @@ crossButton.addEventListener('click', closeWindow);
 
 let closeButton = document.querySelector(".button_note");
 closeButton.addEventListener('click', closeWindow);
+
+//window resize
+console.log(window.outerWidth);
+window.addEventListener('resize', function(){
+    console.log(window.outerWidth);
+    if(window.outerWidth < 768){
+        document.querySelector('.D').innerHTML='DD';
+        document.querySelector('.H').innerHTML='HH';
+        document.querySelector('.M').innerHTML='MM';
+        document.querySelector('.S').innerHTML='SS';
+    }
+    if(window.outerWidth > 768){
+        document.querySelector('.D').innerHTML='Days';
+        document.querySelector('.H').innerHTML='Hours';
+        document.querySelector('.M').innerHTML='Minutes';
+        document.querySelector('.S').innerHTML='Seconds';
+    }
+})
